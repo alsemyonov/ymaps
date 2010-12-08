69cf9c4b19ddf1678a1e9e9486f14c0f8e2946de
Alexander Semyonov <al@semyonov.us>
2010-12-08 11:49:08 +0300

    Fixed Geokit::Inflector#url_escape implementation for Ruby 1.8 and 1.9 compatibility

--- lib/geokit/inflector.rb
+++ lib/geokit/inflector.rb
@@ -2,10 +2,21 @@ require 'geokit/geocoders'
 
 module Geokit
   module Inflector
-    if defined?(::Rack)
-      def url_escape(s)
-          Rack::Utils::escape(s)
+    if ''.respond_to?(:bytesize)  # Ruby 1.9
+      def bytesize(string)
+        string.bytesize
       end
+    else                          # Ruby 1.8
+      def bytesize(string)
+        string.size
+      end
+    end
+    module_function :bytesize
+
+    def url_escape(s)
+      s.gsub(/([^ a-zA-Z0-9_.-]+)/nu) {
+        '%' + $1.unpack('H2' * bytesize($1)).join('%').upcase
+      }.tr(' ', '+')
     end
   end
 end
